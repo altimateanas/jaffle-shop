ac72d5400e54b6e6ee91217052855cf0b96cce8e
Muhammad Anas Farooqui <anas@altimate.ai>
2026-01-15 16:49:22 +0530

    third commit

--- tpch_snowflake_queries/development_query.sql
+++ tpch_snowflake_queries/development_query.sql
@@ -1,9 +1,3 @@
--- Optimized Query: Removed functions from filter conditions
--- Original query: tpch_snowflake_queries/query_4.sql
--- Optimization opportunities identified and fixed:
--- 1. Removed CAST functions from JOIN condition (direct numeric join)
--- 2. Replaced YEAR() function with date range filter for better partition pruning
-
 SELECT DISTINCT
     c_name,
     c_custkey,
@@ -21,7 +15,7 @@ FROM
     SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.REGION
 WHERE
     c_custkey = o_custkey
-    AND l_orderkey = o_orderkey
+    AND CAST(l_orderkey AS VARCHAR) = CAST(o_orderkey AS VARCHAR)
     AND c_nationkey = n_nationkey
     AND n_regionkey = r_regionkey
     AND r_name IN ('ASIA', 'EUROPE')
@@ -30,8 +24,7 @@ WHERE
     AND o_totalprice > (
         SELECT AVG(o2.o_totalprice)
         FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1000.ORDERS o2
-        WHERE o2.o_orderdate >= '1994-01-01'
-        AND o2.o_orderdate < '1995-01-01'
+        WHERE YEAR(o2.o_orderdate) = 1994
     )
 GROUP BY
     c_name,
